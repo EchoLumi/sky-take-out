140f68675206404480a62db17ae9f06d4d951d2a
EchoLumi <18076111486@163.com>
2025-08-22 23:10:10 +0800

    添加：注释

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -18,6 +18,7 @@ public interface EmployeeMapper {
 
     /**
      * 插入员工数据
+     * MyBatis内容
      * @param employee
      */
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
