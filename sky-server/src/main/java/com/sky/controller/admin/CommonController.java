78806c84c26c1f60848c1ccaa16c039c32f44288
EchoLumi <18076111486@163.com>
2025-09-05 23:44:15 +0800

    添加:公共Controllrer类文件，TODO未书写完成

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,24 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 通用
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+    @PostMapping()
+    public Result<String> upload(MultipartFile file){
+        log.info("");
+        return null;
+    }
+}
