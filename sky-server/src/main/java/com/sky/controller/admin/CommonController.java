7d8c3090558fec35ecfe630c405c98e7958c24c6
EchoLumi <18076111486@163.com>
2025-09-06 22:29:17 +0800

    修改：公共配置类文件

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
- * 通用
+ * 通用接口
  */
 @RestController
 @RequestMapping("/admin/common")
 @Api(tags = "通用接口")
 @Slf4j
 public class CommonController {
-    @PostMapping()
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
     public Result<String> upload(MultipartFile file){
-        log.info("");
+        log.info("文件上传：{}",file);
         return null;
     }
 }
