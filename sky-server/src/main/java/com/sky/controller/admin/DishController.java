ac36876dab16f002a4461bbedd308d76c198040f
EchoLumi <18076111486@163.com>
2025-09-09 22:44:58 +0800

    添加：菜品控制类的缺失的注释

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,11 +1,15 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +28,12 @@ public class DishController {
      * @param dishDTO
      * @return
      */
+    @PostMapping
+    @ApiOperation("新增菜品")
     public Result save(DishDTO dishDTO){
         log.info("新增新的菜品：{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
         return Result.success();
     }
+
 }
