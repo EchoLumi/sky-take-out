54683bd1c1d7863d983e883d6830f71bd263c2c4
EchoLumi <18076111486@163.com>
2025-09-18 10:50:12 +0800

    添加：RedisConfiguration类文件

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,25 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建Redis模板对象......");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        // 设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // Redis的序列号器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
